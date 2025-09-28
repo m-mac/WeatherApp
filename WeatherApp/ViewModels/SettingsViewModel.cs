4923914fa1d0c732c5bd742fc93921f926c49a79
Mike Mac <13826357+m-mac@users.noreply.github.com>
2025-09-28 10:10:30 +0800

    feat(settings): implement SettingsPage UI

--- WeatherApp/ViewModels/SettingsViewModel.cs
+++ WeatherApp/ViewModels/SettingsViewModel.cs
@@ -1,11 +1,27 @@
+using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using WeatherApp.Interfaces;
 
 namespace WeatherApp.ViewModels;
 
 public class SettingsViewModel : ObservableObject
 {
-    public SettingsViewModel()
+    private ISettingsService settingsService;
+
+    public IAsyncRelayCommand<string> OpenUrlCommand { get; }
+
+    public SettingsViewModel(ISettingsService settingsService)
     {
-        
+        this.settingsService = settingsService;
+        this.OpenUrlCommand = new AsyncRelayCommand<string>(ExecuteOpenUrlCommand);
+    }
+
+    private static async Task ExecuteOpenUrlCommand(string? url)
+    {
+        if (string.IsNullOrWhiteSpace(url)) return;
+
+        // Should open the default web view 
+        await Launcher.OpenAsync(url);
     }
 }
