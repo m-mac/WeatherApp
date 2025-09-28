d0de3c08206aa8422403ae31887dfa2c083193e6
Mike Mac <13826357+m-mac@users.noreply.github.com>
2025-09-28 13:12:29 +0800

    feat(settings): add set API key UI

--- WeatherApp/Pages/SettingsPage.xaml.cs
+++ WeatherApp/Pages/SettingsPage.xaml.cs
@@ -8,4 +8,18 @@ public partial class SettingsPage
     {
         InitializeComponent();
     }
+
+    // EntryCell doesn't have a Command property, so I'm using the 'Completed' event
+    private void ApiKeyEntryCell_OnCompleted(object? sender, EventArgs e)
+    {
+        if (!(BindingContext is SettingsViewModel viewModel)) return;
+        if (!(sender is EntryCell entryCell)) return;
+        var text = entryCell.Text;
+    
+        if (viewModel.SetApiKeyCommand.CanExecute(null))
+            viewModel.SetApiKeyCommand.Execute(text);
+        
+        // Clear it after sending
+        entryCell.Text = string.Empty;
+    }
 }
