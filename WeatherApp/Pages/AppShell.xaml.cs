2ab6126ee7e7b15d5b3b658bfc9c5d7e1a254653
Mike Mac <13826357+m-mac@users.noreply.github.com>
2025-09-28 15:16:48 +0800

    feat(shell): add OnAppearing method to intialise the app after dependency resolution

--- WeatherApp/Pages/AppShell.xaml.cs
+++ WeatherApp/Pages/AppShell.xaml.cs
@@ -9,4 +9,15 @@ public partial class AppShell : Shell
 		BindingContext = viewModel;
 		InitializeComponent();
 	}
+	
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+
+		if (BindingContext is AppShellViewModel viewModel)
+		{
+			// Initialize first-run tasks on appearing to avoid blocking the UI during construction
+			viewModel.HandleFirstRunAsync();
+		}
+	}
 }
