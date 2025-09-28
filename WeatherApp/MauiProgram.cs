fcdfb9c85e2dcd882a0c2924dca1f5dceaa8c33b
Mike Mac <13826357+m-mac@users.noreply.github.com>
2025-09-28 10:13:21 +0800

    fix: add missing imports in MauiProgram.cs

--- WeatherApp/MauiProgram.cs
+++ WeatherApp/MauiProgram.cs
@@ -1,54 +1,59 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MauiIcons.FontAwesome.Solid;
+using Microsoft.Extensions.Logging;
+using WeatherApp.Interfaces;
 using WeatherApp.Pages;
+using WeatherApp.Services;
 using WeatherApp.ViewModels;
 
 namespace WeatherApp;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.RegisterServices()
-			.RegisterViewModels()
-			.RegisterViews()
-			.UseFontAwesomeSolidMauiIcons()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				fonts.AddFont("fa-solid-900.ttf", "FontAwesomeSolid");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .RegisterServices()
+            .RegisterViewModels()
+            .RegisterViews()
+            .UseFontAwesomeSolidMauiIcons()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("fa-solid-900.ttf", "FontAwesomeSolid");
+            });
+
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 
-	public static MauiAppBuilder RegisterServices(this MauiAppBuilder builder)
-	{
-		builder.Services.AddTransient<ISettingsService, SettingsService>();
-		
-		return builder;
-	}
+    public static MauiAppBuilder RegisterServices(this MauiAppBuilder builder)
+    {
+        builder.Services.AddTransient<ISettingsService, SettingsService>();
 
-	public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder builder)
-	{
-		builder.Services.AddSingleton<MainViewModel>();
-		builder.Services.AddSingleton<SettingsViewModel>();
-		
-		return builder;
-	}
+        return builder;
+    }
 
-	public static MauiAppBuilder RegisterViews(this MauiAppBuilder builder)
-	{
-		builder.Services.AddSingleton<MainPage>();
-		builder.Services.AddSingleton<SettingsPage>();
-		
-		return builder;
-	}
+    public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder builder)
+    {
+        builder.Services.AddSingleton<MainViewModel>();
+        builder.Services.AddSingleton<SettingsViewModel>();
+
+        return builder;
+    }
+
+    public static MauiAppBuilder RegisterViews(this MauiAppBuilder builder)
+    {
+        builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddSingleton<SettingsPage>();
+
+        return builder;
+    }
 }
