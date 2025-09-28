6535d9305902e6fdb9fceb38099ad7840115ed5f
Mike Mac <13826357+m-mac@users.noreply.github.com>
2025-09-29 05:12:39 +0800

    feat(modal): add modal navigation methods to ModalService

--- WeatherApp/Interfaces/IModalService.cs
+++ WeatherApp/Interfaces/IModalService.cs
@@ -4,4 +4,8 @@ public interface IModalService
 {
     Page? GetCurrentPage();
     Task<bool> ShowConfirmationAsync(string title, string message, string acceptText, string cancelText);
+    Task<string?> ShowActionSheetAsync(string title, string cancel, string? destruction, params string[] buttons);
+    Task PushModalAsync(Page page);
+    Task PopModalAsync();
+
 }
--- WeatherApp/Models/ModalService.cs
+++ WeatherApp/Models/ModalService.cs
@@ -4,7 +4,6 @@ namespace WeatherApp.Models;
 
 public class ModalService : IModalService
 {
-
     public Page? GetCurrentPage()
     {
         return Application.Current?
@@ -14,14 +13,37 @@ public class ModalService : IModalService
     }
     public async Task<bool> ShowConfirmationAsync(string title, string message, string acceptText, string? cancelText)
     {
-        var page = GetCurrentPage();
-        if (page == null) return false;
+        var currentPage = GetCurrentPage();
+        if (currentPage == null) return false;
 
         if (cancelText != null)
         {
-            return await page.DisplayAlert(title, message, acceptText, cancelText);
+            return await currentPage.DisplayAlert(title, message, acceptText, cancelText);
         }
         
-        return await page.DisplayAlert(title, message, acceptText, "OK");
+        return await currentPage.DisplayAlert(title, message, acceptText, "OK");
+    }
+
+    public async Task PushModalAsync(Page page)
+    {
+        var currentPage = GetCurrentPage();
+        if (currentPage == null) return;
+
+        await currentPage.Navigation.PushModalAsync(page);
+    }
+
+    public async Task<string?> ShowActionSheetAsync(string title, string cancel, string? destruction, params string[] buttons)
+    {
+        var currentPage = GetCurrentPage();
+        if (currentPage == null) return null;
+
+        return await currentPage.DisplayActionSheet(title, cancel, destruction, buttons);
+    }
+    
+    public async Task PopModalAsync()
+    {
+        var currentPage = GetCurrentPage();
+        if (currentPage == null) return;
+        await currentPage.Navigation.PopModalAsync();
     }
 }
