8d936900d2025da6d54a548772df83d062fb3312
Mike Mac <13826357+m-mac@users.noreply.github.com>
2025-09-28 10:21:03 +0800

    feat(core): add Constants class for database configuration

---
+++ WeatherApp/Constants.cs
@@ -0,0 +1,16 @@
+namespace WeatherApp;
+
+public static class Constants
+{
+    public const string DatabaseFilename = "WeatherApp.db3";
+    
+    // Creates DB if it doesn't exist and enables multithreaded access
+    public const SQLite.SQLiteOpenFlags Flags =
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        SQLite.SQLiteOpenFlags.Create |
+        SQLite.SQLiteOpenFlags.SharedCache;
+    
+    
+    public static string DatabasePath => 
+        Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+}
