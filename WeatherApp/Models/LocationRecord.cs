a4ef7e3bc2c631e6b546c96f72f0f6bdb7f0a19a
Mike Mac <13826357+m-mac@users.noreply.github.com>
2025-09-28 13:15:03 +0800

    refactor(db): update LocationRecord

--- WeatherApp/Models/LocationRecord.cs
+++ WeatherApp/Models/LocationRecord.cs
@@ -7,8 +7,8 @@ public class LocationRecord
     [PrimaryKey,  AutoIncrement]
     public int Id { get; set; }
     
-    public required string Name { get; set; }
-    public required string Address { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
     
     public double Latitude { get; set; }
     public double Longitude { get; set; }
@@ -21,6 +21,9 @@ public class LocationRecord
     public bool Pinned { get; set; }
     
     
+    /// <summary>
+    /// Gets the current weather record for this location. A refresh is triggered if it's missing or out of date. 
+    /// </summary>
     [Ignore]
     public WeatherRecord? CurrentWeatherRecord { get; set; }
 }
